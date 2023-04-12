3c96f5a338955072289565a00bd1bfbd9510ce6d
Mirziyod_ms <sunatillayevmirziyod363@gmail.com>
2023-04-12 13:34:11 +0300

    login page finished

--- Houzing2/Controllers/UserAuthenticationController.cs
+++ Houzing2/Controllers/UserAuthenticationController.cs
@@ -65,24 +65,31 @@ namespace Houzing2.Controllers
         //return Ok(result.Message);
 
         //[HttpGet]
-        public IActionResult Login()
+        public IActionResult Index()
         {
             return View();
-
         }
+
         [HttpPost]
         public async Task<IActionResult> Login(LoginModel model)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
                 return View(model);
+
             var result = await authService.LoginAsync(model);
             if (result.StatusCode == 1)
                 return RedirectToAction("Index", "Home");
             else
             {
                 TempData["msg"] = "Could not logged in..";
-                return RedirectToAction(nameof(Login));
+                return RedirectToAction("Index", "UserAuthentication");
             }
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await authService.LogoutAsync();
+            return RedirectToAction(nameof(Login));
         }
     }
 }
